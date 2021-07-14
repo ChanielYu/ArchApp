5b58a2a54f24550ccc425081172efb3b34618d81
Chaniel Yu <chaniel.yu@gmail.com>
2021-07-14 13:59:41 +1000

    viewbingding

--- ArchitectureApp/src/main/java/com/auxy/archapp/main/ui/notifications/NotificationsFragment.kt
+++ ArchitectureApp/src/main/java/com/auxy/archapp/main/ui/notifications/NotificationsFragment.kt
@@ -4,21 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.auxy.archapp.R
+import com.auxy.archapp.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
-
     private val notificationsViewModel: NotificationsViewModel by viewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
+    private var _binding: FragmentNotificationsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         notificationsViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
+            binding.textNotifications.text = it
         })
-        return root
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentNotificationsBinding.inflate(inflater, container, false).also {
+        _binding = it
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
